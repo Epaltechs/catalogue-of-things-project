3ca50eba38d8e61c419199934ab84eb395903784
yusufnviiri <yusufnviiri@gmail.com>
2022-08-17 04:51:28 +0300

    Add game and author tests

---
+++ spec/authors_spec.rb
@@ -0,0 +1,25 @@
+reqiure './game'
+reqiure './author'
+describe Author do
+  before :all do
+    @author = Author.new('J.K. Rowling', 'Harry Potter')
+    @game = Game.new('Jackson', '2008-01-11', '2005-09-05')
+  end
+  context 'when creating an instance of the class Author' do
+    it 'it takes two parameters and return a Label object' do
+      expect(@author).to be_an_instance_of Author
+    end
+  end
+  context 'When retrieving the first name of author' do
+    it 'it returns the first name of the author' do
+      expect(@author.first_name).to be_an_instance_of Author
+    end
+  end
+  context 'when attaching a game to an author' do
+    it 'adds the game to a specific author' do
+      @author.add_game(@game)
+      expect(@author.games.item).to include(@game)
+      expect(@author.games.item.length).to eq(1)
+    end
+  end
+end
---
+++ spec/games_spec.rb
@@ -0,0 +1,17 @@
+reqiure './game'
+reqiure './author'
+describe Game do
+  before :each do
+    @game = Game.new('Jackson', Date.parse('2008-01-11'), Date.parse('2005-09-05'))
+  end
+  context 'when creating an instance of the class Game' do
+    it 'it takes three parameters and return a Game object' do
+      expect(@game).to be_an_instance_of Game
+    end
+  end
+  context 'when archiving a game' do
+    it 'it returns true if the game can be archived' do
+      expect(@game.can_be_archived?).to be false
+    end
+  end
+end
