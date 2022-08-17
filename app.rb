9ee0814e130507c1cd96ee343f7bbde0f0dc65aa
Arturgouveia1970 <artgouveiag@gmail.com>
2022-08-17 15:55:40 +0100

    corrected app

--- app.rb
+++ app.rb
@@ -64,57 +64,6 @@ class App
     puts
     puts 'There are no books to show! Please add a book.' if @all_books.empty?
 
-  def run
-    puts 'Welcome To The Catalog of My Things!'
-    puts
-    puts
-    until display_list
-      input = gets.chomp
-      if input == '10'
-        puts 'Thank you! :)'
-        break
-      end
-      option(input)
-    end
-  end
-  def create_book
-    print 'Publisher: '
-    book_publisher = gets.chomp
-
-    print 'Cover State: '
-    book_cover_state = gets.chomp
-
-    print 'Label title: '
-    book_label_title = gets.chomp
-
-    print 'Label color: '
-    book_label_color = gets.chomp
-
-    print 'Publish date (DD-MM-YEAR): '
-    book_publish_date = gets.chomp
-
-    puts
-
-    label = Label.new book_label_title, book_label_color
-
-    mybook = Book.new book_publisher, book_cover_state, book_publish_date
-
-    @all_books << mybook
-    @all_labels << Label
-
-    label.add_item(mybook)
-
-    save_book(book_publisher, book_cover_state, book_label_title, book_label_color, book_publish_date)
-
-    puts 'Book created successfully.'
-    puts
-    sleep 0.75
-  end
-
-  def list_books
-    puts
-    puts 'There are no books to show! Please add a book.' if @all_books.empty?
-
     @all_books.each do |book|
       puts '--------------------'
       puts "ID: #{book.id}"
