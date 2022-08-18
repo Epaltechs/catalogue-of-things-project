193af562d031eeab983ecad6df38816e417c87e5
Arturgouveia1970 <artgouveiag@gmail.com>
2022-08-18 11:40:24 +0100

    linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -14,13 +14,15 @@ describe Book do
   end
 
   it 'The can_be_achived? method returns true if difference between current date
+
     and publish date is greater than 10 and cover_state equals to "bad"' do
     @book_one.move_to_archive
 
-    expect(@book_one.archived).to be false
+    expect(@book_one.archived).to be true
   end
 
   it 'The can_be_achived? method returns true if difference between current
+
     date and publish date is greater than 10 and cover_state equals to "good"' do
     @book_two.move_to_archive
 
@@ -28,6 +30,7 @@ describe Book do
   end
 
   it 'The can_be_achived? method returns false if difference between current
+
     date and publish date is less than 10 and cover_state equals to "good"' do
     @book_three.move_to_archive
 
@@ -35,6 +38,7 @@ describe Book do
   end
 
   it 'The can_be_achived? method returns false if difference between current
+
     date and publish date is less than 10 and cover_state equals to "bad"' do
     @book_four.move_to_archive
 
